93d3a8bc330e5ac01a41873e388169d5acb0948b
Zimeng Yang <zimengyang8@gmail.com>
2026-01-16 21:54:29 -0500

    Initial Commit

---
+++ Elvis/ElvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisApp.swift
+//  Elvis
+//
+//  Created by ZM on 1/16/26.
+//
+
+import SwiftUI
+
+@main
+struct ElvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
