58cf10be3c35ff906cd1dd85bfc7b7411ad97ee0
Zimeng Yang <zimengyang8@gmail.com>
2026-01-17 23:44:43 -0500

    Peace, love, understanding App

--- Elvis/ContentView.swift
+++ Elvis/ContentView.swift
@@ -8,12 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var title = ""
+    @State private var imageTitle = ""
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("What's So Funny 'Bout")
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .foregroundStyle(.purple)
+            
+            Spacer()
+
+            Image(systemName: imageTitle)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 300, height: 300)
+            
+            Text(title)
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .foregroundStyle(.purple)
+            
+            Spacer()
+            
+            HStack {
+                Button("Peace") {
+                    title = "Peace"
+                    imageTitle = "peacesign"
+                }
+                
+                Button("Love") {
+                    title = "Love"
+                    imageTitle = "heart"
+                }
+                
+                Button("Understanding") {
+                    title = "Understanding"
+                    imageTitle = "lightbulb"
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            .tint(.purple)
+            
         }
         .padding()
     }
